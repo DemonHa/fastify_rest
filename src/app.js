80e1cc437ef24e321ce47ea131afe6b270e3a081
User <hh360284@gmail.com>
2022-06-15 15:07:28 +0200

    Adding swagger documentation

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
+const { default: fastifySwagger } = require("@fastify/swagger");
 const fastify = require("fastify");
 
 const { todo } = require("./routes/todo");
 
-const build = (opts = {}) => {
+const build = (opts = {}, optSwagger = {}) => {
   const app = fastify(opts);
 
+  app.register(fastifySwagger, optSwagger);
   app.register(todo, { prefix: "/todo" });
   return app;
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 const { build } = require("./app");
 
-const app = build({ logger: true });
+const app = build(
+  { logger: true },
+  {
+    exposeRoute: true,
+    routePrefix: "/docs",
+    swagger: {
+      info: {
+        title: "Swagger Docs",
+        description: "Testing the Fastify swagger API",
+        version: "0.1.0",
+      },
+    },
+  }
+);
 
 app.listen({ port: 3000 }, function (err, address) {
   if (err) {
