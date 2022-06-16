58260250277d1837e2a684ad48e7c9629dab4357
User <hh360284@gmail.com>
2022-06-16 16:19:15 +0200

    Small fix

--- __test__/vat_calculator.test.js
+++ __test__/vat_calculator.test.js
@@ -1,19 +1,19 @@
 const { vatcalculator } = require("./../src/utils/vat_calculator");
 
 describe("VAT calculator", () => {
-  test("", () => {
+  test("Should return vat excluded amount", () => {
     // 16.67
     const result = vatcalculator.calculateVat(16.67);
     expect(result).toBe(3.33);
   });
 
-  test("", () => {
+  test("Should return corrent gross amount of 20% vat", () => {
     // 16.67
     const result = vatcalculator.calculateGrossAmount(16.67);
     expect(result).toBe(20);
   });
 
-  test("", () => {
+  test("Should return corrent net amount for 20% vat", () => {
     // 20
     const result = vatcalculator.calculateNetAmount(20);
     expect(result).toBe(16.67);
